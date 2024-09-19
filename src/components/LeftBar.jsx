27a66e8ffdfa1a212c43685198e599112c6b7c5b
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-19 17:29:49 +0000

    initialized, title splash

---
+++ src/components/LeftBar.jsx
@@ -0,0 +1,8 @@
+import TitleSplash from "./TitleSplash";
+import "./LeftBar.css"
+
+export default function LeftBar() {
+    return <div id="hreddit_leftbar">
+        <TitleSplash />
+    </div>
+}
---
+++ src/components/TitleSplash.jsx
@@ -0,0 +1,6 @@
+export default function TitleSplash() {
+    return <div id="hreddit_titlesplash">
+        <img src="/reddit_icon.jpg" alt="reddit logo"></img>
+        <h4>HREDDIT</h4>
+    </div>
+}
