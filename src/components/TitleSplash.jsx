a2a8c492fe7893d4e00f282ce1f12f361dab0249
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-19 17:32:46 +0000

    title h4 noselect

--- src/components/TitleSplash.jsx
+++ src/components/TitleSplash.jsx
@@ -1,6 +1,6 @@
 export default function TitleSplash() {
     return <div id="hreddit_titlesplash">
         <img src="/reddit_icon.jpg" alt="reddit logo"></img>
-        <h4>HREDDIT</h4>
+        <h4 className="noselect">HREDDIT</h4>
     </div>
 }
