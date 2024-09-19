234d18e9c7cb1e97886e469ffb891b1ac08af0aa
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-19 17:50:17 +0000

    searchbar added

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,8 +1,10 @@
 import TitleSplash from "./TitleSplash";
 import "./LeftBar.css"
+import SearchBar from "./SearchBar";
 
 export default function LeftBar() {
     return <div id="hreddit_leftbar">
         <TitleSplash />
+        <SearchBar />
     </div>
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import "./SearchBar.css"
+
+export default function SearchBar() {
+    return <div id="hreddit_searchbar">
+        <input type="text" id="hreddit_searchbar_searchinput" placeholder="Search Subreddits"></input>
+        <div id="hreddit_searchbar_searchlistwrapper">
+            
+        </div>
+    </div>
+}
