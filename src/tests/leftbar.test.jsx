788943c4bfa3b76a9440d4ebcc1354030cbe6cba
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-20 13:29:38 +0000

    title splash test

---
+++ src/tests/leftbar.test.jsx
@@ -0,0 +1,17 @@
+const { render, screen } = require("@testing-library/react");
+import { Provider } from "react-redux";
+import store from "../features/store.js";
+import '@testing-library/jest-dom';
+import LeftBar from "../components/LeftBar";
+
+test('loads splash, input and subreddits', async () => {
+    render(<Provider store={store}><LeftBar /></Provider>);
+    
+    const redditLogo = screen.getByRole('img', {name: "reddit logo"});
+    const subredditSearch = screen.getByPlaceholderText(/Search Subreddits/i)
+    const homeSubreddit = screen.getByText('r/Loading subreddits...');
+
+    expect(redditLogo).toBeInTheDocument();
+    expect(subredditSearch).toBeInTheDocument();
+    expect(homeSubreddit).toBeInTheDocument();
+});
