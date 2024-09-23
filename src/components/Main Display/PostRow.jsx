17e4a8e0565d5e2034b0ed160d2f36ebc3ec6fd3
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-23 14:07:45 +0000

    post details, styling

--- src/components/Main Display/PostRow.jsx
+++ src/components/Main Display/PostRow.jsx
@@ -1,5 +1,12 @@
 export default function PostRow(props) {
-    return <div className="PostRow">
+
+    return <div className="hreddit_postrow">
         <h4>{props.postDetails.title}</h4>
+        <div className="hreddit_postrow_detailsdiv">
+            <p>{props.postDetails.score}</p>
+            <p>{props.postDetails.subreddit_name_prefixed}</p>
+            <p>{props.postDetails.num_comments} comments</p>
+            <p>{props.postDetails.author}</p>
+        </div>
     </div>
 }
