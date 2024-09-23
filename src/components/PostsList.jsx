f2fcbcbfe522febb785b06028056e3eef21da934
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-23 12:53:32 +0000

    styling

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,10 +2,14 @@ import { useSelector } from "react-redux";
 import { selectPosts } from "../features/frontPageSlice";
 import PostRow from "./PostRow";
 
+import "./PostsList.css";
+
 export default function PostsList() {
     const loadedPosts = useSelector(selectPosts);
 
-    return <div id="hreddit_postslist">
-        {loadedPosts.map((x) => <PostRow postDetails={x}/>)}
+    return <div id="hreddit_postslist_wrapper">
+        <div id="hreddit_postslist">
+            {loadedPosts.map((x, i) => <PostRow postDetails={x} key={i}/>)}
+        </div>
     </div>
 }
