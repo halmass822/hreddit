512be675efe6bbfc58b3c8f9c2d528c00794a821
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-23 18:11:57 +0000

    post preview

--- src/components/MainDisplay/PostRow.jsx
+++ src/components/MainDisplay/PostRow.jsx
@@ -1,7 +1,20 @@
 export default function PostRow(props) {
 
+    if(props.postDetails.post_hint === "image") console.log(props.postDetails.thumbail_height);
+
+    function displayData() { //determines what type of media (if any) and displays it
+        switch(props.postDetails.post_hint) {
+            case "image":
+                return <img className="hreddit_postlist_post_image" src={props.postDetails.thumbnail} height={props.postDetails.thumbnail_height} width={props.postDetails.thumbnail_width}></img>
+                break;
+            default:
+                break;
+        }
+    }
+
     return <div className="hreddit_postrow">
         <h4>{props.postDetails.title}</h4>
+        {displayData()}
         <div className="hreddit_postrow_detailsdiv">
             <p>{props.postDetails.score}</p>
             <p>{props.postDetails.subreddit_name_prefixed}</p>
