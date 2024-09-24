72bd2e7649b2ce685a707f58aadaac3a5147a047
halmass822 <halmassri822@gmail.com>
2024-09-24 12:34:06 -0400

    icons modified

--- src/components/LeftBar/TitleSplash.jsx
+++ src/components/LeftBar/TitleSplash.jsx
@@ -1,6 +1,6 @@
 export default function TitleSplash() {
     return <div id="hreddit_titlesplash">
-        <img src="/reddit_icon.jpg" alt="reddit logo"></img>
+        <img src="./reddit_icon.png" alt="reddit logo"></img>
         <h4 className="noselect">HREDDIT</h4>
     </div>
 }
