8f805cd636756d3393440a8ba994ce4840afe4f4
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-27 15:29:43 +0000

    top panel selected filter fixed

--- src/components/MainDisplay/TopPanel.jsx
+++ src/components/MainDisplay/TopPanel.jsx
@@ -9,15 +9,17 @@ export default function TopPanel() {
     const currentSubreddit = useSelector(selectCurrentSubreddit);
     const currentFilter = useSelector(selectCurrentFilter);
 
+    console.log(currentFilter);
+
     function handleButtonClick(input) {
         dispatch(setSelectedFilter(input));
         dispatch(getPostsBySubreddit([currentSubreddit, input]));
     }
 
     return <div id="hreddit_topPanel">
-        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("hot")} className={currentFilter === "hot" && "hreddit_selectedfilter"}>Hot</button>
-        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("new")} className={currentFilter === "new" && "hreddit_selectedfilter"}>New</button>
-        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("top")} className={currentFilter === "top" && "hreddit_selectedfilter"}>Top</button>
-        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("rising")} className={currentFilter === "rising" && "hreddit_selectedfilter"}>Rising</button>
+        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("hot")} className={currentFilter === "hot" ? "hreddit_selectedfilter" : null}>Hot</button>
+        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("new")} className={currentFilter === "new" ? "hreddit_selectedfilter" : null}>New</button>
+        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("top")} className={currentFilter === "top" ? "hreddit_selectedfilter" : null}>Top</button>
+        <button id="hreddit_topPanel_hot" onClick={() => handleButtonClick("rising")} className={currentFilter === "rising" ? "hreddit_selectedfilter" : null}>Rising</button>
     </div>
 }
