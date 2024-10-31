64781f164eee27a9cdc6a469d301926acd0ae569
halmass822 <93576140+halmass822@users.noreply.github.com>
2024-10-31 13:22:23 +0000

    modal content

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./Modal.css";
 import { selectLoadingPostState, selectOverlayState, selectPostDetails, setOverlayState } from "../../features/postSlice";
+import _ from "lodash";
 
 export default function Modal() {
 
@@ -16,9 +17,13 @@ export default function Modal() {
     return <div id="hreddit_modal_wrapper" hidden={!overlayState}>
         <div id="hreddit_modal_backdrop" onClick={() => dispatch(setOverlayState(false))}></div>
         <div id="hreddit_modal_main">
-            {loadingPostState ? <img id="hreddit_modal_loadstateimg" alt="spinning reddit logo" src="./reddit_icon.png"></img> : <>
+            {loadingPostState || _.isEmpty(postDetails) ? <img id="hreddit_modal_loadstateimg" alt="spinning reddit logo" src="./reddit_icon.png"></img> : <>
                     <div id="hreddit_modal_content">
+                        <div id="hreddit_modal_content_info">
+                            <p>{postDetails[0].data.children[0].data.author}</p>
+                        </div>
                         <h4>{postDetails[0].data.children[0].data.title}</h4>
+                        {<img src={postDetails[0].data.children[0].data.url} alt="reddit_post_image"></img>}
                     </div>
                     <div id="hreddit_modal_comments">
                     </div>
@@ -27,3 +32,4 @@ export default function Modal() {
     </div>
     
 }
+
