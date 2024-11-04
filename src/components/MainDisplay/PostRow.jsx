692a02f00cf043663e5f404ad421a234b8934eb7
halmass822 <93576140+halmass822@users.noreply.github.com>
2024-11-04 19:22:34 +0000

    default thumbnail in postrow

--- src/components/MainDisplay/PostRow.jsx
+++ src/components/MainDisplay/PostRow.jsx
@@ -1,17 +1,13 @@
 export default function PostRow(props) {
 
-    function displayData() { //determines what type of media (if any) and displays it
-        switch(props.postDetails.post_hint) {
-            case "image":
-                return <img className="hreddit_postlist_post_image" src={props.postDetails.thumbnail} height={props.postDetails.thumbnail_height} width={props.postDetails.thumbnail_width}></img>
-            default:
-                break;
-        }
-    }
-
     return <div className="hreddit_postrow" onClick={() => props.openPostProp(props.postDetails.permalink)}>
         <div className="hreddit_postrow_titleandpreview">
-        {displayData()}
+
+        {props.postDetails.thumbnail === "default" || props.postDetails.thumbnail === "self" || /external-preview/.test(props.postDetails.thumbnail)
+        ? <img id="hreddit_postrow_thumbnaildefault" alt="placeholder reddit logo" src="./reddit_icon.png" height={140} width={140}></img>
+        : <img className="hreddit_postlist_post_image" src={props.postDetails.thumbnail} height={props.postDetails.thumbnail_height} width={props.postDetails.thumbnail_width}></img> 
+        }
+
         <h4>{props.postDetails.title}</h4>
         </div>
         <div className="hreddit_postrow_detailsdiv">
